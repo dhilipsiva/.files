c26cad787b887c8f5881a400d6b532d727d5a9f8
dhilipsiva <dhilipsiva@gmail.com>
2017-09-25 16:13:36 +0530

    Minor re-aggangement

--- common/exports.sh
+++ common/exports.sh
@@ -1,6 +1,13 @@
-export EDITOR='nvim'
+# independant vars (Depends on static and builtins)
 export DOTFILES_DIR=$HOME/.files
-export XDG_CONFIG_HOME=$DOTFILES_DIR/.config
-export TMUXP_CONFIGDIR=$DOTFILES_DIR/.tmuxp
-export INPUTRC="$XDG_CONFIG_HOME"/readline/inputrc
+export EDITOR='nvim'
+export GOPATH=$(go env GOPATH)
 export PATH=/usr/local/bin:$PATH
+
+# 1st Degree Derivatives (Depends only on Independant vars)
+export PATH=$GOPATH/bin:$PATH
+export TMUXP_CONFIGDIR=$DOTFILES_DIR/.tmuxp
+export XDG_CONFIG_HOME=$DOTFILES_DIR/.config
+
+# 2dn Degree Derivatives (Depends on 1st Degree variables)
+export INPUTRC=$XDG_CONFIG_HOME/readline/inputrc
