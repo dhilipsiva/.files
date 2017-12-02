b6b266a19657e34a42c0cfa992aa6a25003cf437
dhilipsiva <dhilipsiva@gmail.com>
2017-12-02 16:55:45 +0530

    Finalize settings

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,7 @@
 from os import environ
 
 from fabric.api import run, env, sudo, task, cd, settings
-from gitric.api import git_seed, git_reset, allow_dirty, force_push
+from gitric.api import git_seed, git_reset, allow_dirty
 
 from fabtools import deb, require
 
@@ -23,18 +23,22 @@ def wget(cmd):
     run("wget %s" % cmd)
 
 
-@task
-def setup():
-    """
+def setup_gui():
     run("/usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2017.01.02_all.deb keyring.deb SHA256:4c3c6685b1181d83efe3a479c5ae38a2a44e23add55e16a328b8c8560bf05e5f") # NOQA
     sudo("dpkg -i ./keyring.deb")
     sudo('echo "deb http://debian.sur5r.net/i3/ $(grep \'^DISTRIB_CODENAME=\' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list')  # NOQA
     deb.update_index()
     deb.upgrade()
     require.deb.packages([
-        "i3", "lightdm", "xfce4-terminal", "build-essential", "python-pip",
-        "unzip"])
-    # run('sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"')
+        "i3", "nodm", "xfce4-terminal", "chromium-browser", "firefox"])
+
+
+@task
+def setup():
+    deb.update_index()
+    deb.upgrade()
+    require.deb.packages(["build-essential", "python-pip", "unzip", "xclip"])
+    run('sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"')  # NOQA
     sudo("pip install -U pip")
     sudo("pip install powerline-shell tmuxp")
     run("touch private.sh")
@@ -46,21 +50,21 @@ def setup():
     run("brew install gcc")
     run("brew install ruby")
     run("brew install python3 neovim bash")
-    run("pip3 install tmuxp powerline-shell pwdman hostscli")
+    run("pip3 install tmuxp powerline-shell pwdman hostscli neovim")
     sudo("hostscli block_all")
     require.files.directories([tmp_dir])
-    """
     with cd(tmp_dir):
-        """
-        Setup Fonts
-        """
+        # Setup Fonts
         branch = "master"
         gh = "https://github.com"
         repos = ["powerline/fonts", "ryanoasis/nerd-fonts"]
         repo_names = [r.split("/")[1] for r in repos]
         for repo, repo_name in zip(repos, repo_names):
-            wget("%s/%s/archive/%s.zip -O %s.zip" % (gh, repo, branch, repo_name))
+            wget("%s/%s/archive/%s.zip -O %s.zip" % (
+                gh, repo, branch, repo_name))
             run("unzip %s.zip" % repo_name)
             with cd("%s-%s" % (repo_name, branch)):
                 with settings(warn_only=True):
                     run("./install.sh")
+    run("curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim") # NOQA
+    run('nvim -c "PlugInstall | q | q"')
