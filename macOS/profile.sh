d5fe0aa48789ca05369fa10085f6632f2722a4e5
dhilipsiva <dhilipsiva@gmail.com>
2017-12-02 19:41:13 +0530

    Comment out some stuff

--- macOS/profile.sh
+++ macOS/profile.sh
@@ -10,3 +10,6 @@ source ~/.files/macOS/exports.sh
 source ~/.files/macOS/aliases.sh
 source ~/.files/macOS/functions.sh
 [ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
+# if [ -f /usr/local/share/bash-completion/bash_completion ]; then
+# . /usr/local/share/bash-completion/bash_completion
+# fi
