417b9c86077def71d79d16b34c7bb6acba0df33d
dhilipsiva <dhilipsiva@gmail.com>
2017-12-02 14:47:26 +0530

    All the way through setting up font

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,7 @@
 from os import environ
 
-from fabric.api import run, env, sudo, task, cd
-from gitric.api import git_seed, git_reset, allow_dirty
+from fabric.api import run, env, sudo, task, cd, settings
+from gitric.api import git_seed, git_reset, allow_dirty, force_push
 
 from fabtools import deb, require
 
@@ -9,12 +9,18 @@ env.user = environ['DOTFILES_USER']
 env.hosts = environ['DOTFILES_HOST']
 allow_dirty = allow_dirty  # Silence flake8
 
-dotfiles_dir = "/home/%s/.files" % env.user
+home_dir = "/home/%s" % env.user
+tmp_dir = "%s/tmp" % home_dir
+dot_dir = "%s/.files" % home_dir
 
 
 @task
 def echo():
     run("echo foo")
+
+
+def wget(cmd):
+    run("wget %s" % cmd)
 
 
 @task
@@ -25,14 +31,36 @@ def setup():
     sudo('echo "deb http://debian.sur5r.net/i3/ $(grep \'^DISTRIB_CODENAME=\' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list')  # NOQA
     deb.update_index()
     deb.upgrade()
-    """
-    require.deb.packages(["i3", "lightdm", "xfce4-terminal", "build-essential", "python-pip"])
+    require.deb.packages([
+        "i3", "lightdm", "xfce4-terminal", "build-essential", "python-pip",
+        "unzip"])
     # run('sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"')
     sudo("pip install -U pip")
     sudo("pip install powerline-shell tmuxp")
     run("touch private.sh")
-    git_seed(dotfiles_dir)
-    git_reset(dotfiles_dir)
-    with cd(dotfiles_dir):
-        run("cp home/.* ~")
+    git_seed(dot_dir)
+    git_reset(dot_dir)
+    with cd(dot_dir):
+        with settings(warn_only=True):
+            run("cp home/.* ~")
     run("brew install gcc")
+    run("brew install ruby")
+    run("brew install python3 neovim bash")
+    run("pip3 install tmuxp powerline-shell pwdman hostscli")
+    sudo("hostscli block_all")
+    require.files.directories([tmp_dir])
+    """
+    with cd(tmp_dir):
+        """
+        Setup Fonts
+        """
+        branch = "master"
+        gh = "https://github.com"
+        repos = ["powerline/fonts", "ryanoasis/nerd-fonts"]
+        repo_names = [r.split("/")[1] for r in repos]
+        for repo, repo_name in zip(repos, repo_names):
+            wget("%s/%s/archive/%s.zip -O %s.zip" % (gh, repo, branch, repo_name))
+            run("unzip %s.zip" % repo_name)
+            with cd("%s-%s" % (repo_name, branch)):
+                with settings(warn_only=True):
+                    run("./install.sh")
