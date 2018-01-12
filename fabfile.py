4dfeac7e5cfb9bfc810fc7d0baa214655ee955ff
dhilipsiva <dhilipsiva@gmail.com>
2018-01-12 23:15:34 +0530

    Make setup_gui as task

--- fabfile.py
+++ fabfile.py
@@ -23,6 +23,7 @@ def wget(cmd):
     run("wget %s" % cmd)
 
 
+@task
 def setup_gui():
     run("/usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2017.01.02_all.deb keyring.deb SHA256:4c3c6685b1181d83efe3a479c5ae38a2a44e23add55e16a328b8c8560bf05e5f") # NOQA
     sudo("dpkg -i ./keyring.deb")
