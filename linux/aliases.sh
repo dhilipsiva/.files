ad4267ea25e8327f91ee9515e38d4d3c6e5071e5
dhilipsiva <dhilipsiva@gmail.com>
2018-06-12 00:19:35 +0530

    RabbitMQ update

--- linux/aliases.sh
+++ linux/aliases.sh
@@ -1,3 +1,3 @@
 alias c='xclip -select c'
 alias pg="postgres -D /home/linuxbrew/.linuxbrew/var/postgres/"
-alias rmq="/home/linuxbrew/.linuxbrew/Cellar/rabbitmq/3.7.0/sbin/rabbitmq-server"
+alias rmq="/home/linuxbrew/.linuxbrew/Cellar/rabbitmq/3.7.5/sbin/rabbitmq-server"
