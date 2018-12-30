41d5225d316e9fc5fb995aa140c08ca334d5aaa8
dhilipsiva <dhilipsiva@pm.me>
2018-12-30 06:45:53 +0530

    Script to resolve curl ssl issue

---
+++ resolve_curl_ssl.sh
@@ -0,0 +1,5 @@
+echo insecure >> ~/.curlrc
+git config --global http.sslVerify false
+HOMEBREW_CURLRC=1 brew reinstall openssl curl
+sed -i '/^insecure$/d' ~/.curlrc
+git config --global http.sslVerify true
